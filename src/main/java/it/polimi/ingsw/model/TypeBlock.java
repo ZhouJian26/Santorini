cb2be4fe3370482a145ec3b0714613593cf389a5
Lily Sana <lisa.zhouxy@gmail.com>
2020-03-30 00:13:41 +0200

    Modified switch cases all into standard condition

--- src/main/java/it/polimi/ingsw/model/TypeBlock.java
+++ src/main/java/it/polimi/ingsw/model/TypeBlock.java
@@ -3,7 +3,9 @@ package it.polimi.ingsw.model;
 public enum TypeBlock {
     LEVEL1, LEVEL2, LEVEL3, DOME, WORKER;
 
-    // check if upblock can to built on downblock
+    // check if upblock can be built on downblock in STANDARD CONDITION
+    // need to consider exceptions with GodPowers
+
     public static boolean allowedLiftUp (TypeBlock downblock, TypeBlock upblock) {
         switch (upblock) {
             case LEVEL1:
@@ -13,7 +15,7 @@ public enum TypeBlock {
             case LEVEL3:
                 return (downblock == TypeBlock.LEVEL2);
             case DOME:
-                return (downblock != TypeBlock.DOME ) && ( downblock != TypeBlock.WORKER);
+                return (downblock == TypeBlock.LEVEL3);
             case WORKER:
                 return (downblock != TypeBlock.WORKER ) && ( downblock != TypeBlock.DOME);
             default:
