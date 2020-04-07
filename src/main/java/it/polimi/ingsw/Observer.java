0dae7b083260e200c8adb51fd076f6f99b479a92
jian zhou <jianzhou@MBP-di-jian.homenet.telecomitalia.it>
2020-04-07 10:49:13 +0200

    first version of game and controller

--- src/main/java/it/polimi/ingsw/Observer.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.view;
-
-public interface Observer<T> {
-
-    public void update(T message);
-
-}
--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.model;
 
+import java.io.ObjectInputFilter.Status;
+
 class Player implements Cloneable {
     private String username;
+    private StatusPlayer status;
+    private Color color;
 
     /**
      * 
@@ -9,11 +13,13 @@ class Player implements Cloneable {
      */
     public Player(String username) {
         this.username = username;
+        status = StatusPlayer.END;
     }
 
     @Override
     public Player clone() {
         Player player = new Player(username);
+        player.setStatusPlayer(status);
         return player;
     }
 
@@ -23,5 +29,23 @@ class Player implements Cloneable {
      */
     public String getUsername() {
         return username;
+    }
+
+    public void setStatusPlayer(StatusPlayer status) {
+        this.status = status;
+    }
+
+    public StatusPlayer getStatusPlayer() {
+        return status;
+    }
+
+    public void setColor(Color color) {
+        if (this.color == null) {
+            this.color = color;
+        }
+    }
+
+    public Color getColor() {
+        return color;
     }
 }
