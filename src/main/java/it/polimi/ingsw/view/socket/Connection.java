e61a0998fba3fc2eb262299f4772a02e272fa14e
jian zhou <jianzhou@MBP-di-jian.homenet.telecomitalia.it>
2020-04-27 16:38:54 +0200

    cli/gui connection and parser start

--- src/main/java/it/polimi/ingsw/view/socket/Connection.java
+++ src/main/java/it/polimi/ingsw/view/socket/Connection.java
@@ -3,44 +3,39 @@ package it.polimi.ingsw.view.socket;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 import it.polimi.ingsw.utils.Observable;
 import it.polimi.ingsw.utils.Observer;
-import it.polimi.ingsw.utils.model.Command;
 
-public class Connection extends Observable<ArrayList<Command>> implements Runnable, Observer<Command> {
-    private final transient String ip;
-    private final transient int port;
-    private transient Socket socket;
-    private transient Scanner receiver;
-    private transient PrintWriter sender;
-    private boolean isActive = false;
+public class Connection extends Observable<String> implements Runnable, Observer<String> {
+    public final transient String ip;
+    public final transient int port;
+    private final transient Socket socket;
+    private final transient Scanner receiver;
+    private final transient PrintWriter sender;
+    private boolean isActive;
 
+    /**
+     * 
+     * @param ip   server
+     * @param port server
+     * @throws IOException
+     */
     public Connection(String ip, int port) throws IOException {
         this.ip = ip;
         this.port = port;
-        connectToServer();
+        this.socket = new Socket(ip, port);
+        this.receiver = new Scanner(socket.getInputStream());
+        this.sender = new PrintWriter(socket.getOutputStream());
+        this.isActive = true;
     }
 
-    private synchronized void connectToServer() {
-        while (!isActive) {
-            try {
-                socket = new Socket(ip, port);
-                receiver = new Scanner(socket.getInputStream());
-                sender = new PrintWriter(socket.getOutputStream());
-                setStatus(true);
-            } catch (IOException e) {
-                setStatus(false);
-            }
-        }
-    }
-
+    /**
+     * 
+     * @param toSend data to send to the server
+     */
     private synchronized void send(String toSend) {
         if (!isActive)
             return;
@@ -48,8 +43,15 @@ public class Connection extends Observable<ArrayList<Command>> implements Runnab
         sender.flush();
     }
 
-    private synchronized void setStatus(boolean status) {
-        this.isActive = status;
+    public boolean getStatus() {
+        return isActive;
+    }
+
+    public void close() throws IOException {
+        isActive = false;
+        sender.close();
+        receiver.close();
+        socket.close();
     }
 
     @Override
@@ -57,19 +59,17 @@ public class Connection extends Observable<ArrayList<Command>> implements Runnab
         try {
             while (isActive) {
                 String serverPush = receiver.nextLine();
-                notify(new Gson().fromJson(serverPush, new TypeToken<ArrayList<Command>>() {
-                }.getType()));
+                notify(serverPush);
             }
         } catch (NoSuchElementException e) {
-            setStatus(false);
-            connectToServer();
+            isActive = false;
         }
     }
 
     @Override
-    public void update(Command command) {
-        if (command == null || command.funcName == null || command.funcName.equals(""))
+    public void update(String toSend) {
+        if (toSend == null)
             throw new NullPointerException();
-        send(new Gson().toJson(command));
+        send(toSend);
     }
 }
--- src/main/java/it/polimi/ingsw/view/socket/Parser.java
+++ src/main/java/it/polimi/ingsw/view/socket/Parser.java
@@ -4,44 +4,86 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
+import it.polimi.ingsw.utils.Observable;
 import it.polimi.ingsw.utils.Observer;
 import it.polimi.ingsw.utils.model.Command;
 
-class Parser implements Observer<ArrayList<Command>> {
+class Parser extends Observable<ArrayList<Command>> implements Observer<String> {
     private ArrayList<Command> commandList;
 
     public Parser() {
         commandList = new ArrayList<Command>();
     }
 
-    private synchronized void setCommandList(ArrayList<Command> commandList) {
-        if (commandList == null) {
-            commandList = new ArrayList<>();
-            return;
-        }
-        this.commandList = commandList;
-    }
-
-    @Override
-    public void update(ArrayList<Command> commandList) {
-        setCommandList(commandList);
-    }
-
-    public ArrayList<String> getFilters() {
-        return (ArrayList<String>) commandList.stream().map(e -> e.type).distinct().collect(Collectors.toList());
-    }
-
-    public ArrayList<Command> getCommandList() {
+    private ArrayList<Command> duplicateCommandList() {
         return new Gson().fromJson(new Gson().toJson(commandList), new TypeToken<ArrayList<Command>>() {
         }.getType());
     }
 
+    private synchronized void setCommandList(ArrayList<Command> commandList) {
+        if (commandList == null)
+            throw new NullPointerException();
+        this.commandList = commandList;
+    }
+
+    @Override
+    public void update(String commandList) {
+        try {
+            setCommandList(new Gson().fromJson(commandList, new TypeToken<ArrayList<Command>>() {
+            }.getType()));
+            notify(duplicateCommandList());
+        } catch (JsonSyntaxException e) {
+        }
+    }
+
+    /**
+     * 
+     * @return an array list of string of actual filter available (type of command)
+     */
+    public ArrayList<String> getFilters() {
+        return (ArrayList<String>) duplicateCommandList().stream().map(e -> e.type).distinct()
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 
+     * @return full array list of commands
+     */
+    public ArrayList<Command> getCommandList() {
+        return duplicateCommandList();
+    }
+
+    /**
+     * 
+     * @param filter a string to filter commands (type field)
+     * @return array list with filtered command (type field)
+     */
     public ArrayList<Command> getCommandList(String filter) {
         if (getFilters().contains(filter))
-            return (ArrayList<Command>) commandList.stream().filter(e -> e.type.equals(filter))
-                    .map(e -> new Gson().fromJson(new Gson().toJson(e), Command.class)).collect(Collectors.toList());
+            return (ArrayList<Command>) duplicateCommandList().stream().filter(e -> e.type.equals(filter))
+                    .collect(Collectors.toList());
         return new ArrayList<Command>();
+    }
+
+    /**
+     * 
+     * @return array list of all commands usable from users
+     */
+    public ArrayList<Command> getUsableCommandList() {
+        return (ArrayList<Command>) duplicateCommandList().stream().filter(e -> {
+            return e.funcName != null;
+        }).collect(Collectors.toList());
+    }
+
+    /**
+     * 
+     * @param command to parse into string
+     * @return command parsed into string
+     */
+    public static String toString(Command command) {
+        return new Gson().toJson(command);
     }
 }
