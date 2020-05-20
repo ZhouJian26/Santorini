50f2a864eb2584c4e007f016f061353f8fa2e5b0
jian zhou <jianzhou@MBP-di-jian.homenet.telecomitalia.it>
2020-05-20 09:50:19 +0200

    module printer

--- src/main/java/it/polimi/ingsw/view/CLI/ViewPrinter.java
+++ src/main/java/it/polimi/ingsw/view/CLI/ViewPrinter.java
@@ -56,20 +56,21 @@ public class ViewPrinter extends Observable<String> implements Observer<ArrayLis
         System.out.flush();
     }
 
-    private void printGameInfo() {
-        List<ArrayList<String>> title = new ArrayList<>();
-        title.add(new ArrayList<>(Arrays.asList("GAME INFO")));
-        printRow(title);
-        List<ArrayList<String>> toPrint = Arrays
+    private ArrayList<String> printGameInfo() {
+        ArrayList<String> toRes = new ArrayList<>();
+        List<ArrayList<String>> title = new ArrayList<>(Arrays.asList(new ArrayList<>(Arrays.asList("GAME INFO"))));
+        // title.add(new ArrayList<>(Arrays.asList("GAME INFO")));
+        toRes.addAll(composeRow(title));
+        toRes.addAll(composeRow(Arrays
                 .asList("Game Mode: " + parser.getGameMode(), "Game Phase: " + parser.getGamePhase(),
                         "Current Player: " + parser.getCurrentPlayer())
-                .stream().map(e -> new ArrayList<>(Arrays.asList(e))).collect(Collectors.toList());
-        printRow(toPrint);
+                .stream().map(e -> new ArrayList<>(Arrays.asList(e))).collect(Collectors.toList())));
+        return toRes;
     }
 
     private String centerFill(int space, String word) {
         String out = String.format("%" + space + "s%s%" + space + "s", "", word, "");
-        return out.substring((out.length() - space) / 2, (out.length() + space) / 2 - 1);
+        return out.substring((out.length() - space) / 2, (out.length() + space) / 2);
     }
 
     private String centerFill(int space, String word, String fill) {
@@ -80,40 +81,86 @@ public class ViewPrinter extends Observable<String> implements Observer<ArrayLis
         return out.substring((out.length() - space) / 2, (out.length() - space) / 2 + space);
     }
 
-    private void printRow(List<ArrayList<String>> toPrint) {
-        int spaceDefault = 120;
+    private void printRow(ArrayList<String> toPrint) {
+        for (String x : toPrint)
+            System.out.println(x);
+    }
+
+    private ArrayList<String> composeRow(ArrayList<String> container_1, ArrayList<String> container_2) {
+        int maxH = Math.max(container_1.size(), container_2.size());
+        ArrayList<String> toRes = new ArrayList<>();
+        for (int index = 0; index < maxH; index++) {
+            String toAdd = "";
+            if (index - (maxH - container_1.size()) / 2 >= 0 && index < (maxH + container_1.size()) / 2)
+                toAdd += container_1.get(index - (maxH - container_1.size()) / 2);
+            else
+                toAdd += String.format("%" + container_1.get(0).length() + "s", "");
+
+            if (index - (maxH - container_2.size()) / 2 >= 0 && index < (maxH + container_2.size()) / 2)
+                toAdd += container_2.get(index - (maxH - container_2.size()) / 2);
+            else
+                toAdd += String.format("%" + container_2.get(0).length() + "s", "");
+            toRes.add(toAdd);
+        }
+        return toRes;
+    }
+
+    private ArrayList<String> composeRow(List<ArrayList<String>> toPrint, int space) {
+        ArrayList<String> toRes = new ArrayList<>();
         int maxH = toPrint.stream().map(e -> e.size()).max(Integer::compare).get();
-        System.out.print(centerFill(spaceDefault + 1, "", "-") + "\n|" + centerFill(spaceDefault, "") + "|\n|");
+        toRes.add(centerFill(space + 1, "", "-"));
+        toRes.add("|" + centerFill(space, "") + "|");
+
         IntStream.range(0, maxH).forEachOrdered(index -> {
+            String toAdd = "|";
             for (ArrayList<String> x : toPrint) {
                 if (index - (maxH - x.size()) / 2 >= 0 && index < (maxH + x.size()) / 2)
-                    System.out.print(
-                            centerFill(spaceDefault / toPrint.size(), x.get(index - (maxH - x.size()) / 2)) + "|");
+                    toAdd += centerFill(space / toPrint.size(), x.get(index - (maxH - x.size()) / 2));
                 else
-                    System.out.print(centerFill(spaceDefault / toPrint.size(), "") + "|");
+                    toAdd += centerFill(space / toPrint.size(), "");
+                toAdd += "|";
             }
-            if (index + 1 < maxH)
-                System.out.print("\n|");
-            else
-                System.out.print("\n|" + centerFill(spaceDefault, "") + "|");
+            toRes.add(toAdd);
         });
-        System.out.println();
+        toRes.add("|" + centerFill(space, "") + "|");
+        return toRes;
     }
 
-    private void printPlayerInfo() {
-        List<ArrayList<String>> title = new ArrayList<>();
-        title.add(new ArrayList<>(Arrays.asList("PLAYERS")));
-        printRow(title);
+    private ArrayList<String> composeRow(List<ArrayList<String>> toPrint) {
+        return composeRow(toPrint, 120);
+    }
+    /*
+     * private void printRow(List<ArrayList<String>> toPrint) { int spaceDefault =
+     * 120; int maxH = toPrint.stream().map(e ->
+     * e.size()).max(Integer::compare).get();
+     * System.out.print(centerFill(spaceDefault + 1, "", "-") + "\n|" +
+     * centerFill(spaceDefault, "") + "|\n|"); IntStream.range(0,
+     * maxH).forEachOrdered(index -> { for (ArrayList<String> x : toPrint) { if
+     * (index - (maxH - x.size()) / 2 >= 0 && index < (maxH + x.size()) / 2)
+     * System.out.print( centerFill(spaceDefault / toPrint.size(), x.get(index -
+     * (maxH - x.size()) / 2)) + "|"); else System.out.print(centerFill(spaceDefault
+     * / toPrint.size(), "") + "|"); } if (index + 1 < maxH)
+     * System.out.print("\n|"); else System.out.print("\n|" +
+     * centerFill(spaceDefault, "") + "|"); }); System.out.println(); }
+     */
+
+    private ArrayList<String> printPlayerInfo() {
+        ArrayList<String> toRes = new ArrayList<>();
+        List<ArrayList<String>> title = new ArrayList<>(Arrays.asList(new ArrayList<>(Arrays.asList("PLAYERS"))));
+        // List<ArrayList<String>> title = new ArrayList<>();
+        // title.add(new ArrayList<>(Arrays.asList("PLAYERS")));
+        toRes.addAll(composeRow(title));
         List<ArrayList<String>> toPrint = parser.getPlayers().stream().map(e -> e.getRawData())
                 .collect(Collectors.toList());
-        printRow(toPrint);
+        toRes.addAll(composeRow(toPrint));
+        return toRes;
     }
 
-    private void printBoardInfo() {
+    private ArrayList<String> printBoardInfo() {
 
-        List<ArrayList<String>> title = new ArrayList<>();
-        title.add(new ArrayList<>(Arrays.asList("BOARD")));
-        printRow(title);
+        ArrayList<String> toRes = new ArrayList<>();
+        List<ArrayList<String>> title = new ArrayList<>(Arrays.asList(new ArrayList<>(Arrays.asList("BOARD"))));
+        toRes.addAll(composeRow(title));
         Cell[][] toPrint = parser.getBoard();
         int position = 0;
         for (Cell[] row : toPrint) {
@@ -124,48 +171,50 @@ public class ViewPrinter extends Observable<String> implements Observer<ArrayLis
                 toPrintRow.add(toPush);
                 position++;
             }
-            printRow(toPrintRow);
+            toRes.addAll(composeRow(toPrintRow));
         }
+        return toRes;
     }
 
-    private void printActionInfo() {
+    private ArrayList<String> printActionInfo() {
         if (username == null || !username.equals(parser.getCurrentPlayer()))
-            return;
-        List<ArrayList<String>> title = new ArrayList<>();
-        title.add(new ArrayList<>(Arrays.asList("ACTIONS", "It's your turn!")));
-        printRow(title);
+            return null;
+        ArrayList<String> toRes = new ArrayList<>();
+        List<ArrayList<String>> title = new ArrayList<>(
+                Arrays.asList(new ArrayList<>(Arrays.asList("ACTIONS", "It's your turn!"))));
+        toRes.addAll(composeRow(title));
         ArrayList<ArrayList<String>> toPrint = (ArrayList<ArrayList<String>>) parser.getUsableCommandList().stream()
                 .map(e -> {
-                    ArrayList<String> toRes;
+                    ArrayList<String> toRet;
                     // System.out.println(new Gson().toJson(e));
                     switch (e.type) {
                         case "action":
                             if (e.info == null)
-                                toRes = new ArrayList<>(Arrays.asList("End Turn"));
+                                toRet = new ArrayList<>(Arrays.asList("End Turn"));
                             else if (new Gson().fromJson(e.info, TypeAction.class).TypeAction.equals("Swap"))
-                                toRes = new Gson().fromJson(e.info, Swap.class).getRawData();
+                                toRet = new Gson().fromJson(e.info, Swap.class).getRawData();
                             else
-                                toRes = new Gson().fromJson(e.info, Build.class).getRawData();
+                                toRet = new Gson().fromJson(e.info, Build.class).getRawData();
                             break;
                         case "board":
-                            toRes = new Gson().fromJson(e.info, Cell.class).getRawData();
-                            toRes.add("[" + Integer.parseInt(e.funcData) / 5 + "," + Integer.parseInt(e.funcData) % 5
+                            toRet = new Gson().fromJson(e.info, Cell.class).getRawData();
+                            toRet.add("[" + Integer.parseInt(e.funcData) / 5 + "," + Integer.parseInt(e.funcData) % 5
                                     + "]");
                             break;
                         case "color":
-                            toRes = new Color(e.info).getRawData();
+                            toRet = new Color(e.info).getRawData();
                             break;
                         case "god":
-                            toRes = new God(e.info).getRawData();
+                            toRet = new God(e.info).getRawData();
                             break;
                         case "godList":
-                            toRes = new God(e.info).getRawData();
+                            toRet = new God(e.info).getRawData();
                             break;
                         default:
-                            toRes = null;
+                            toRet = null;
                             break;
                     }
-                    return toRes;
+                    return toRet;
                 }).filter(e -> e != null && e.size() > 0).collect(Collectors.toList());
         int indexAction = 0;
         while (toPrint.size() > 0) {
@@ -175,9 +224,10 @@ public class ViewPrinter extends Observable<String> implements Observer<ArrayLis
             for (ArrayList<String> x : rowToPrint)
                 x.add(0, "Action: " + (indexAction + i++));
             indexAction += rowToPrint.size();
-            printRow(rowToPrint);
+            toRes.addAll(composeRow(rowToPrint));
             toPrint.subList(0, toPrint.size() < 5 ? toPrint.size() : 5).clear();
         }
+        return toRes;
     }
 
     private String getActionString(int index) {
@@ -196,20 +246,21 @@ public class ViewPrinter extends Observable<String> implements Observer<ArrayLis
         return true;
     }
 
-    private void printGeneralInfo() {
-        printGameInfo();
-        printPlayerInfo();
-    }
-
     private void printView() {
         if (!needUpdate || !status)
             return;
         needUpdate = false;
         clearConsole();
-        printGeneralInfo();
-        printBoardInfo();
-        printActionInfo();
-        System.out.println(centerFill(121, "", "-"));
+        ArrayList<String> gameInfo, Actions;
+        gameInfo = printGameInfo();
+        gameInfo.addAll(printPlayerInfo());
+        gameInfo.addAll(printBoardInfo());
+        Actions = printActionInfo();
+        if (Actions != null)
+            printRow(composeRow(gameInfo, Actions));
+        else
+            printRow(gameInfo);
+        // System.out.println(centerFill(121, "", "-"));
     }
 
     @Override
