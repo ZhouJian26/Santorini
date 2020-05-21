7268ca7732716af456289343a3fa9901f39d653d
Lily Sana <lisa.zhou@mail.polimi.it>
2020-05-21 21:10:18 +0200

    Changes to the inital page

--- src/main/java/it/polimi/ingsw/view/GUI/InitialPageController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/InitialPageController.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.view.socket.Connection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -19,10 +20,13 @@ public class InitialPageController {
     private URL location;
 
     @FXML
-    private TextField ip, port;
+    private TextField ip, port, username;
 
     @FXML
-    private Button connect;
+    private Button connect, playButton;
+
+    @FXML
+    private ChoiceBox<?> modes;
 
     @FXML
     /**
@@ -33,6 +37,7 @@ public class InitialPageController {
             try{
                 connection = new Connection(ip.getText(), Integer.parseInt(port.getText()));
                 System.out.println("Connected");
+                changeScene();
 
             }catch (Exception e){
 
@@ -40,6 +45,33 @@ public class InitialPageController {
         }
 
     }
+
+    @FXML
+    void sendPlayerInfo(ActionEvent event) {
+        System.out.println("ok");
+    }
+
+    @FXML
+    private void initialize(){
+        ip.setVisible(true);
+        port.setVisible(true);
+        connect.setVisible(true);
+        modes.setVisible(false);
+        username.setVisible(false);
+        playButton.setVisible(false);
+    }
+
+    private void changeScene(){
+        ip.setVisible(false);
+        port.setVisible(false);
+        connect.setVisible(false);
+        modes.setVisible(true);
+        username.setVisible(true);
+        playButton.setVisible(true);
+    }
+
+
+
 
 
 
