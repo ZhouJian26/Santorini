29785617d84f81a38ff6df016de642104081b87c
xuzewei <xuzewei@Doodecp.local>
2020-05-29 01:01:50 +0200

    fix

--- src/main/java/it/polimi/ingsw/model/GodHera.java
+++ src/main/java/it/polimi/ingsw/model/GodHera.java
@@ -7,11 +7,11 @@ public class GodHera extends GodDecorator {
 
     @Override
     public void getEvent(Event[] events, Cell[][] map, Action[][][] actions) {
-        System.out.println("0");
+
         if (events[0].equals(Event.MOVE) && !godPower.getName().equals(godPower.getCurrentPlayer())) {
-            System.out.println("1");
+
             if (godPower.getPositionWorker()[0] == 0 || godPower.getPositionWorker()[0] == 4 || godPower.getPositionWorker()[1] == 0 || godPower.getPositionWorker()[1] == 4) {
-                System.out.println("2");
+
                 if (godPower.getPlayerStatus().equals(StatusPlayer.WIN)) {
                     godPower.setStatusPlayer(StatusPlayer.GAMING);
                     godPower.setLastGod(God.HERA);
