54ab50b561a1b14ecc05ec70b9873c5954bf8c07
xuzewei <xuzewei@Doodecp.local>
2020-06-30 18:28:33 +0200

    fix

--- src/main/java/it/polimi/ingsw/model/GodAthena.java
+++ src/main/java/it/polimi/ingsw/model/GodAthena.java
@@ -38,8 +38,8 @@ class GodAthena extends GodDecorator {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 if ((map[i][j].getSize() > map[getPositionWorker()[0]][getPositionWorker()[1]].getSize())
-                        || (map[i][j].getSize() == map[getPositionWorker()[0]][getPositionWorker()[1]].getSize()
-                                && !map[i][j].getBlock(map[i][j].getSize()).getTypeBlock().equals(TypeBlock.WORKER))) {
+                        || ((map[i][j].getSize() == map[getPositionWorker()[0]][getPositionWorker()[1]].getSize()
+                                && !map[i][j].getBlock(map[i][j].getSize()).getTypeBlock().equals(TypeBlock.WORKER)))) {
                     actions[i][j][0].set(false);
                     actions[i][j][0].setGod(God.ATHENA);
                     actions[i][j][0].setBlocked(true);
